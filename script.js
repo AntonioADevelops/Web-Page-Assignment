0b8304fe78256745c9a05d9841a415ec51c2a3f3
AntonioADevelops <ayalarantonio16@gmail.com>
2022-01-25 21:27:32 -0800

    device scaling

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//////////////////////////Device Scaling////////////////////////////////
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 ///////////////////////////Fourth Image/////////////////////////////////
 var modal = document.getElementById("myModalD");
 var img = document.getElementById("myImgD");
